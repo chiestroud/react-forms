78831ec655a169db0f901664a5220a74ff7670ed
Chie Stroud <urirunrunkoko@gmail.com>
2021-04-26 17:40:17 -0500

    pushing up all the changes

--- src/App/index.js
+++ src/App/index.js
@@ -1,38 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
+import StudentForm from '../StudentForm';
+import { getStudent } from '../helpers/data/StudentData';
+import StudentCard from '../components/StudentCard';
 
 function App() {
-  const [domWriting, setDomWriting] = useState('Nothing Here!');
+  const [students, setStudents] = useState([]);
 
-  const handleClick = (e) => {
-    console.warn(`You clicked ${e.target.id}`);
-    setDomWriting(`You clicked ${e.target.id}! Check the Console!`);
-  };
+  useEffect(() => {
+    getStudent().then((response) => setStudents(response));
+  }, []);
 
   return (
     <div className='App'>
-      <h2>INSIDE APP COMPONENT</h2>
-      <div>
-        <button
-          id='this-button'
-          className='btn btn-info'
-          onClick={handleClick}
-        >
-          I am THIS button
-        </button>
-      </div>
-      <div>
-        <button
-          id='that-button'
-          className='btn btn-primary mt-3'
-          onClick={handleClick}
-        >
-          I am THAT button
-        </button>
-      </div>
-      <h3>{domWriting}</h3>
+      <StudentForm
+        formTitle='Form Title'
+        setStudents={setStudents}
+      />
+      <hr />
+      {students.map((studentInfo) => (
+        <StudentCard key={studentInfo.firebaseKey}
+          name={studentInfo.name}
+          teacher={studentInfo.teacher}
+          grade={Number(studentInfo.grade)}
+          handleClick={() => console.warn(`${studentInfo.name}'s teacher is ${studentInfo.teacher}`)}
+        />
+      ))}
     </div>
   );
 }
+
+/* <Card body key={studentInfo.firebaseKey}>
+<CardTitle tag="h5">{studentInfo.name}</CardTitle>
+<CardText>Teacher: {studentInfo.teacher}</CardText>
+<CardText>Grade: {studentInfo.grade}</CardText>
+<Button onClick={() => console.warn(`${studentInfo.name}'s teacher is ${studentInfo.teacher}`)}>Print Student</Button>
+</Card> */
 
 export default App;
---
+++ src/StudentForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { addStudent } from './helpers/data/StudentData';
+
+const StudentForm = ({ formTitle, setStudents }) => {
+  const [student, setStudent] = useState({
+    name: '',
+    teacher: '',
+    grade: 0
+  });
+
+  const handleInputChange = (e) => {
+    setStudent((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.name === 'grade' ? Number(e.target.value) : e.target.value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addStudent(student).then((studentsArray) => setStudents(studentsArray));
+  };
+
+  return (
+    <>
+      <div className='student-form'>
+        <form
+          id='addStudentForm'
+          autoComplete='off'
+          onSubmit={handleSubmit}
+        >
+          <h2>{formTitle}</h2>
+          <label>Name: </label>
+          <input
+            name='name'
+            type='text'
+            placeholder='Name'
+            value={student.name}
+            onChange={handleInputChange}
+          ></input>
+          <label>Teacher: </label>
+          <input
+            name='teacher'
+            type='text'
+            placeholder='Teacher'
+            value={student.teacher}
+            onChange={handleInputChange}
+          ></input>
+          <label>Grade: </label>
+          <input
+            name='grade'
+            type='number'
+            placeholder='Grade'
+            value={student.grade}
+            onChange={handleInputChange}
+          ></input>
+          <button type='submit'>Submit</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+StudentForm.propTypes = {
+  formTitle: PropTypes.string.isRequired,
+  setStudents: PropTypes.func.isRequired
+};
+
+export default StudentForm;
